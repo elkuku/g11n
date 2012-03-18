4a69ceddb9509f83d72722edf9cf3e1b774c66f0
Nikolai Plath <der.el.kuku@gmail.com>
2012-03-17 20:32:28 -0500

    Code style

--- library/g11n/language/javascript/language.js
+++ library/g11n/language/javascript/language.js
@@ -1,167 +1,163 @@
 /**
- * @version SVN: $Id$
- */
-
-/**
  * JavaScript g11n object. This has been "stolen" from the Joomla! 1.6 code
  * base - to improve it =;)
- * 
+ *
  * Custom behavior for JavaScript I18N in Joomla! 1.6
- * 
+ *
  * Allows you to call Joomla.JText._() to get a translated JavaScript string
  * pushed in with JText::script() in Joomla.
  */
-(function() {
-	/**
-	 * 
-	 */
-	g11n = {
-		strings : {},
-		stringsPlural : {},
-		legacy : '',
-		debug : '',
+(function () {
+    /**
+     *
+     */
+    g11n = {
+        strings:{},
+        stringsPlural:{},
+        legacy:'',
+        debug:'',
 
-		pluralFunction : '',
+        pluralFunction:'',
 
-		/**
-		 * 
-		 */
-		loadLanguageStrings : function(object) {
-			for(var key in object) {
-				this.strings[key] = object[key];
-			}
-		},
+        /**
+         *
+         */
+        loadLanguageStrings:function (object) {
+            for (var key in object) {
+                this.strings[key] = object[key];
+            }
+        },
 
-		/**
-		 * 
-		 */
-		loadPluralStrings : function(object) {
-			for(var key in object) {
-				this.stringsPlural[key] = object[key];
-			}
-		},
+        /**
+         *
+         */
+        loadPluralStrings:function (object) {
+            for (var key in object) {
+                this.stringsPlural[key] = object[key];
+            }
+        },
 
-		/**
-		 * 
-		 */
-		setPluralFunction : function(object) {
-			this.pluralFunction = object;
-		},
+        /**
+         *
+         */
+        setPluralFunction:function (object) {
+            this.pluralFunction = object;
+        },
 
-		/**
-		 * 
-		 */
-		translate : function(string) {
-			if (this.debug) {
-				return this.debugTranslate(string);
-			}
+        /**
+         *
+         */
+        translate:function (string) {
+            if (this.debug) {
+                return this.debugTranslate(string);
+            }
 
-			test = phpjs.md5(string);
+            test = phpjs.md5(string);
 
-			if (typeof this.strings[test] !== 'undefined') {
-				return phpjs.base64_decode(this.strings[test]);
-			}
+            if (typeof this.strings[test] !== 'undefined') {
+                return phpjs.base64_decode(this.strings[test]);
+            }
 
-			test = phpjs.md5(string.toUpperCase());
+            test = phpjs.md5(string.toUpperCase());
 
-			if (this.legacy == 'mixed' || !this.legacy) {
-				if (typeof this.strings[test] !== 'undefined') {
-					return phpjs.base64_decode(this.strings[test]);
-				}
-			}
+            if (this.legacy == 'mixed' || !this.legacy) {
+                if (typeof this.strings[test] !== 'undefined') {
+                    return phpjs.base64_decode(this.strings[test]);
+                }
+            }
 
-			return string;
-		},
+            return string;
+        },
 
-		/**
-		 * 
-		 */
-		translatePlural : function(singular, plural, count) {
-			key = phpjs.md5(singular);
+        /**
+         *
+         */
+        translatePlural:function (singular, plural, count) {
+            key = phpjs.md5(singular);
 
-			index = phpjs.call_user_func(this.pluralFunction, count);
+            index = phpjs.call_user_func(this.pluralFunction, count);
 
-			if (typeof this.stringsPlural[key] !== 'undefined') {
-				if (typeof this.stringsPlural[key][index] !== 'undefined') {
-					return phpjs.base64_decode(this.stringsPlural[key][index]);
-				}
-			}
+            if (typeof this.stringsPlural[key] !== 'undefined') {
+                if (typeof this.stringsPlural[key][index] !== 'undefined') {
+                    return phpjs.base64_decode(this.stringsPlural[key][index]);
+                }
+            }
 
-			// -- Fallback - english: singular == 1
-			return (count == 1) ? singular : plural;
-		},
+            // -- Fallback - english: singular == 1
+            return (count == 1) ? singular : plural;
+        },
 
-		/**
-		 * 
-		 */
-		debugTranslate : function(string) {
-			test = selfhtml.MD5(string);
-			// console.log(test);
-			// console.log(phpjs.md5(string));
+        /**
+         *
+         */
+        debugTranslate:function (string) {
+            test = selfhtml.MD5(string);
+            // console.log(test);
+            // console.log(phpjs.md5(string));
 
-			if (typeof this.strings[test] !== 'undefined') {
-				var msg = phpjs.sprintf('Translated:\nO: %s\nT: %s', string,
-						phpjs.base64_decode(this.strings[test]));
-				var add = this.log(msg);
+            if (typeof this.strings[test] !== 'undefined') {
+                var msg = phpjs.sprintf('Translated:\nO: %s\nT: %s', string,
+                    phpjs.base64_decode(this.strings[test]));
+                var add = this.log(msg);
 
-				return phpjs.sprintf(add, phpjs
-						.base64_decode(this.strings[test]));
-			}
+                return phpjs.sprintf(add, phpjs
+                    .base64_decode(this.strings[test]));
+            }
 
-			if (this.legacy == 'mixed' || !this.legacy) {
-				test = selfhtml.MD5(string.toUpperCase());
-				if (typeof this.strings[test] !== 'undefined') {
-					var msg = phpjs.sprintf('Legacy:\nO: %s\nT: %s', string,
-							phpjs.base64_decode(this.strings[test]));
-					var add = this.log(msg, 'info');
+            if (this.legacy == 'mixed' || !this.legacy) {
+                test = selfhtml.MD5(string.toUpperCase());
+                if (typeof this.strings[test] !== 'undefined') {
+                    var msg = phpjs.sprintf('Legacy:\nO: %s\nT: %s', string,
+                        phpjs.base64_decode(this.strings[test]));
+                    var add = this.log(msg, 'info');
 
-					return phpjs.sprintf(add, phpjs
-							.base64_decode(this.strings[test]));
-				}
-			}
+                    return phpjs.sprintf(add, phpjs
+                        .base64_decode(this.strings[test]));
+                }
+            }
 
-			var add = this.log(phpjs.sprintf('Untranslated:\nO: %s', string),
-					'warn');
-			this.log('', 'trace');
+            var add = this.log(phpjs.sprintf('Untranslated:\nO: %s', string),
+                'warn');
+            this.log('', 'trace');
 
-			return phpjs.sprintf(add, string);
-		},
+            return phpjs.sprintf(add, string);
+        },
 
-		/**
-		 * 
-		 */
-		log : function(string, type) {
+        /**
+         *
+         */
+        log:function (string, type) {
 
-			var add = '%s';
+            var add = '%s';
 
-			switch (type) {
-			case 'info':// legacy
-				add = 'L-%s-L';
-				if (typeof (console) != 'undefined')
-					console.info(string);
-				break;// untranslated
-			case 'warn':
-				add = '¿-%s-¿';
-				if (typeof (console) != 'undefined') {
-					console.warn(string);
-					console.log(phpjs.md5(string));
-				}
-				break;
-			case 'trace':
-				if (typeof (console) != 'undefined')
-					console.trace('Trace');
-				break;
-			case undefined:// translated/other
-			default:
-				add = '+-%s-+';
-				if (typeof (console) != 'undefined')
-					console.log(string);
-				break;
+            switch (type) {
+                case 'info':// legacy
+                    add = 'L-%s-L';
+                    if (typeof (console) != 'undefined')
+                        console.info(string);
+                    break;// untranslated
+                case 'warn':
+                    add = '¿-%s-¿';
+                    if (typeof (console) != 'undefined') {
+                        console.warn(string);
+                        console.log(phpjs.md5(string));
+                    }
+                    break;
+                case 'trace':
+                    if (typeof (console) != 'undefined')
+                        console.trace('Trace');
+                    break;
+                case undefined:// translated/other
+                default:
+                    add = '+-%s-+';
+                    if (typeof (console) != 'undefined')
+                        console.log(string);
+                    break;
 
-			}
+            }
 
-			return add;
-		}
-	}// g11n
+            return add;
+        }
+    }// g11n
 
 })();
--- library/g11n/language/javascript/methods.js
+++ library/g11n/language/javascript/methods.js
@@ -1,29 +1,23 @@
 /**
- * @version SVN: $Id$
- */
-
-/**
  * Small multilanguaging function =;).
- * 
+ *
  * Also includes sprintf() functionality if more parameters are supplied.
- * 
+ *
  * @param string original The original string to translate
  * @param additionals [n] if additional paramaters are supplied, the function behaves like sprintf.
- * 
+ *
  * @returns string The translated string or the original if not found.
  */
-function jgettext(original) 
-{
-	var translation = g11n.translate(original);
+function jgettext(original) {
+    var translation = g11n.translate(original);
 
-	if(arguments.length > 1) 
-	{
-		arguments[0] = translation;
-		
-		return phpjs.call_user_func_array(phpjs.sprintf, arguments);
-	}
+    if (arguments.length > 1) {
+        arguments[0] = translation;
 
-	return translation;
+        return phpjs.call_user_func_array(phpjs.sprintf, arguments);
+    }
+
+    return translation;
 }//function
 
 /**
@@ -34,7 +28,6 @@ function jgettext(original)
  *
  * @return string Translated text.
  */
-function jngettext(singular, plural, count)
-{
+function jngettext(singular, plural, count) {
     return g11n.translatePlural(singular, plural, count);
 }//function
