10cae84ad5f8008ba7e70d96e9ed44f78c16dd67
Nikolai Plath <der.el.kuku@gmail.com>
2013-06-22 18:36:38 -0500

    Add parser base classes

---
+++ library/g11n/Language/Parser/Code.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @copyright  2010-2013 Nikolsi Plath
+ * @license    GNU/GPL http://www.gnu.org/licenses/gpl.html
+ */
+
+namespace g11n\Language\Parser;
+
+/**
+ * Class Code.
+ *
+ * @package g11n\Language\Parser
+ */
+abstract class Code
+{
+	/**
+	 * File extension.
+	 *
+	 * @var string
+	 */
+	protected $ext;
+
+	/**
+	 * Get the extension.
+	 *
+	 * @return string
+	 */
+	public function getExt()
+	{
+		return $this->ext;
+	}
+
+	/**
+	 * Convert to string.
+	 *
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return (string)__CLASS__;
+	}
+
+	/**
+	 * Set the language format.
+	 *
+	 * @param string $langFormatIn The language format e.g. ini
+	 *
+	 * @return void
+	 */
+	abstract public function setLangFormat($langFormatIn);
+
+	/**
+	 * Parse a file.
+	 *
+	 * @param string $fileName File to parse
+	 *
+	 * @return object g11nFileInfo
+	 */
+	abstract public function parse($fileName);
+}
---
+++ library/g11n/Language/Parser/Language.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @copyright  2010-2013 Nikolsi Plath
+ * @license    GNU/GPL http://www.gnu.org/licenses/gpl.html
+ */
+
+namespace g11n\Language\Parser;
+
+use g11n\Support\FileInfo;
+
+/**
+ * Class Language.
+ *
+ * Base class for language file parsers.
+ */
+abstract class Language
+{
+	/**
+	 * File extension.
+	 *
+	 * @var string
+	 */
+	protected $ext;
+
+	/**
+	 * Get the extension.
+	 *
+	 * @return string
+	 */
+	public function getExt()
+	{
+		return $this->ext;
+	}
+
+	/**
+	 * Convert to string.
+	 *
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return (string)__CLASS__;
+	}
+
+	/**
+	 * Parse a po style language file.
+	 *
+	 * @param string $fileName Absolute path to the language file.
+	 *
+	 * @return FileInfo
+	 */
+	abstract public function parse($fileName);
+
+	/**
+	 * Generate a language file.
+	 *
+	 * @param FileInfo $fileInfo
+	 * @param \stdClass      $options JObject
+	 *
+	 * @return string
+	 */
+	abstract public function generate(FileInfo $fileInfo, $options);
+}
