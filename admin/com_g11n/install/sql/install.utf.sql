45608491f8f7be4ce46692c70e09c46edaba9c20
Nikolai Plath <der.el.kuku@gmail.com>
2016-07-14 02:31:37 -0500

    Delete obsolete files

--- admin/com_g11n/install/sql/install.utf.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `#__g11n` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `scope` varchar(5) NOT NULL,
-  `extension` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
--- admin/com_g11n/install/sql/uninstall.utf.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS `#__g11n`;
