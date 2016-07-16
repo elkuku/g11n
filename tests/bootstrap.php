f119ebf8b44ceeb1a8b46e8097251c582ead2bf7
Nikolai Plath <der.el.kuku@gmail.com>
2016-07-16 08:29:48 -0500

    [ut] update bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,16 +14,13 @@ ini_set('display_errors', 1);
 /*
  * Ensure that required path constants are defined.
  */
-defined('JPATH_ROOT') || define('JPATH_ROOT', realpath(dirname(__DIR__)));
-defined('JPATH_CONFIGURATION') || define('JPATH_CONFIGURATION', JPATH_ROOT . '/etc');
-defined('JPATH_THEMES') || define('JPATH_THEMES', JPATH_ROOT . '/www');
-defined('JPATH_TEMPLATES') || define('JPATH_TEMPLATES', JPATH_ROOT . '/templates');
+define('TEST_ROOT', realpath(dirname(__DIR__)));
 
-$composerPath = JPATH_ROOT . '/vendor/autoload.php';
+$composerPath = TEST_ROOT . '/vendor/autoload.php';
 
 if (!file_exists($composerPath))
 {
 	throw new RuntimeException('Composer is not set up, please run "composer install".');
 }
 
-require JPATH_ROOT . '/vendor/autoload.php';
+require TEST_ROOT . '/vendor/autoload.php';
