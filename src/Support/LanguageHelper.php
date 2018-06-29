024f124d1026349e97ac0c5718796a0012c7ad62
Nikolai Plath <der.el.kuku@gmail.com>
2018-06-29 13:28:44 -0500

    Add a LanguageHelper class

---
+++ src/Support/LanguageHelper.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: test
+ * Date: 29/06/18
+ * Time: 13:24
+ */
+
+namespace ElKuKu\G11n\Support;
+
+
+class LanguageHelper
+{
+	/**
+	 * List of known languages.
+	 *
+	 * @var array
+	 */
+	private static $languages = [];
+
+	/**
+	 * Get the defined direction for a language.
+	 *
+	 * @param   string  $languageCode  The language code e.g. en-GB
+	 *
+	 * @return string 'ltr' or 'rtl'. Defaults to 'ltr'
+	 */
+	public static function getDirection($languageCode)
+	{
+		if (array_key_exists($languageCode, static::$languages))
+		{
+			return static::$languages[$languageCode]['direction'] ?? 'ltr';
+		}
+
+		return 'ltr';
+	}
+}
