9e8e4c578818b11a7c1bed2be55a16ed1a1ec41a
Nikolai Plath <der.el.kuku@gmail.com>
2018-06-29 14:08:33 -0500

    Add more methods

--- src/Support/LanguageHelper.php
+++ src/Support/LanguageHelper.php
@@ -1,15 +1,17 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: test
- * Date: 29/06/18
- * Time: 13:24
+ * @copyright  2018 Nikolai Plath
+ * @license    GNU/GPL http://www.gnu.org/licenses/gpl.html
  */
 
 namespace ElKuKu\G11n\Support;
 
-
-class LanguageHelper
+/**
+ * Class LanguageHelper
+ * @package ElKuKu\G11n\Support
+ * @since 1.0
+ */
+abstract class LanguageHelper
 {
 	/**
 	 * List of known languages.
@@ -21,7 +23,7 @@ class LanguageHelper
 	/**
 	 * Get the defined direction for a language.
 	 *
-	 * @param   string  $languageCode  The language code e.g. en-GB
+	 * @param   string $languageCode The language code e.g. en-GB
 	 *
 	 * @return string 'ltr' or 'rtl'. Defaults to 'ltr'
 	 */
@@ -33,5 +35,56 @@ class LanguageHelper
 		}
 
 		return 'ltr';
+	}
+
+	/**
+	 * Get a language tag by code.
+	 *
+	 * @param   string $languageCode The language code.
+	 *
+	 * @return string
+	 */
+	public static function getLanguageTagByCode($languageCode)
+	{
+		return array_key_exists($languageCode, static::$languages) ? static::$languages[$languageCode]['iso'] : '';
+	}
+
+	/**
+	 * Get an array with language codes (e.g. en-GB)
+	 *
+	 * @return array
+	 */
+	public static function getLanguageCodes()
+	{
+		return array_keys(self::$languages);
+	}
+
+	/**
+	 * Get an array containing information about languages.
+	 *
+	 * @return array
+	 */
+	public static function getLanguages()
+	{
+		return self::$languages;
+	}
+
+	/**
+	 * Get an array containing information about languages.
+	 * Sorted by display name.
+	 *
+	 * @return array
+	 */
+	public static function getLanguagesSortedByDisplayName()
+	{
+		$languages = self::$languages;
+
+		uasort(
+			$languages, function ($a, $b) {
+				return strcmp($a['display'], $b['display']);
+			}
+		);
+
+		return $languages;
 	}
 }
